b5c24cc0e12513cadd88b0c03a59bb4d22dee0a7
Jordan Halfman <jordanhalfman@gmail.com>
2022-09-07 14:54:26 -0700

    commit tree 5095f5bd189acfeb8a60ce4e7ca88634da779f78

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+    new_array = []
+    if array.length == 2
+        new_array = array.join(" and ")
+    elsif array.length > 2
+        array.each_with_index do |n, index|
+            if index == array.length - 1
+                new_array << "and #{n}"
+            else
+                new_array << n
+            end   
+        end
+        new_array.join(", ")
+    else
+        new_array = array[0]
+    end
 
 end
+
+oxford_comma(["hello", "good evening", "goodbye"])
